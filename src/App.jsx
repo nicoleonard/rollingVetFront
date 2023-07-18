36bf3065a295b4a9c1512f3dcf0341a9bea24b40
Santiago Antich <santiantich98@gmail.com>
2023-07-17 23:45:48 -0300

    Agregué las rutas y componentes para registro, login, inicio, inicio-admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import './App.css'
-import Menu from './components/common/Menu'
+import './App.css';
+import Menu from './components/common/Menu';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Registro from './components/views/Registro';
+import Login from './components/views/Login';
+import Inicio from './components/views/Inicio';
+import InicioAdmin from './components/views/InicioAdmin';
 
 function App() {
   return (
-    <>
-    <Menu />
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Menu></Menu>}></Route>
+        <Route exact path="/registro" element={<Registro></Registro>}></Route>
+        <Route exact path="/login" element={<Login></Login>}></Route>
+        <Route exact path="/inicio" element={<Inicio></Inicio>}></Route>
+        <Route exact path="/inicio-admin" element={<InicioAdmin></InicioAdmin>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
---
+++ src/components/views/InicioAdmin.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InicioAdmin = () => {
+    return (
+        <div>
+            <h1>Inicio Admin</h1>
+        </div>
+    );
+};
+
+export default InicioAdmin;
