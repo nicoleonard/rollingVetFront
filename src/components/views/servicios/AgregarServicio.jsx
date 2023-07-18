1ec69ae42734788a1ff408df49ad0a1fa51b1d94
nicoleonard <nikolasleonard@gmail.com>
2023-07-18 19:49:21 -0300

    Agregado componente para crear nuevos servicios

---
+++ src/components/views/servicios/AgregarServicio.jsx
@@ -0,0 +1,80 @@
+import { Form, Button, Container } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import { AgregarServicio } from "../../helpers/queriesServicios";
+
+const AgregarServicio = () => {
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+
+    const onSubmit = (nuevoServicio) => {
+        agregarServicio(nuevoServicio).then((respuesta) => {
+            if (respuesta && respuesta.status === 201) {
+                Swal.fire('Operacion exitosa', `Se ha creado el servicio ${nuevoServicio.nombre}`, 'success');
+                reset();
+            } else {
+                Swal.fire('Oops... algo salio mal', `${nuevoServicio.nombre} no pudo ser agregado como un nuevo servicio, quizás luego`, 'error');
+            }
+        });
+
+    };
+
+    return (
+        <Container>
+            <h1 className="display-4 mt-5">Nuevo servicio</h1>
+            <hr />
+            <Form onSubmit={handleSubmit(onSubmit)}>
+                <Form.Group className="mb-3">
+                    <Form.Label>Nombre</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Ej: Peluqueria, Nutricion, UTI..."
+                        {...register("nombre", {
+                            required: "El nombre del servicio no puede estar vacio", minLength: { value: 5, message: "El nombre del servicio debe tener al menos 5 caracteres" }, maxLength: { value: 100, message: "El nombre del servicio puede tener como maximo 100 caracteres" },
+                        })}
+                    />
+                    <Form.Text className="text-danger">
+                        {errors.nombre?.message}
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Descripcion</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Ej: Corte de pelo, lavado de orejas..."
+                        {...register("descripcion", {
+                            required: "La descripcion del servicio no puede estar vacia", minLength: { value: 12, message: "La descripcion del servicio debe tener al menos 12 caracteres" }, maxLength: { value: 200, message: "La descripcion del servicio puede tener como maximo 300 caracteres" },
+                        })}
+                    />
+                    <Form.Text className="text-danger">
+                        {errors.descripcion?.message}
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Precio</Form.Label>
+                    <Form.Control
+                        type="number"
+                        placeholder="Ej: 50"
+                        {...register("precio", {
+                            required: "El precio del servicio no puede estar vacio",
+                            min: {
+                                value: 10,
+                                message: "El precio minimo es de $10",
+                            },
+                            max: {
+                                value: 50000,
+                                message: "El precio maximo es de $50000",
+                            },
+                        })}
+                    />
+                    <Form.Text className="text-danger">
+                        {errors.precio?.message}
+                    </Form.Text>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Guardar
+                </Button>
+            </Form>
+        </Container>
+    );
+};
+
+export default AgregarServicio;
