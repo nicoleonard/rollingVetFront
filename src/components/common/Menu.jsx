ead8bfaed1c8097aa10f1ca4244393ff34839f2b
Santiago Antich <santiantich98@gmail.com>
2023-07-18 23:02:19 -0300

    Enlacé el navbar con las rutas definidas en el componente app

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,21 +1,25 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link, NavLink } from 'react-router-dom';
 
 function Menu() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar bg='danger' variant='dark' expand='lg'>
       <Container>
-        <Navbar.Brand href="#home">RollingVet</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">RollingVet</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Inicio</Nav.Link>
-            <Nav.Link href="#link">Registro</Nav.Link>
-            <Nav.Link href="#link">Administrador</Nav.Link>
-            <Nav.Link href="#link">Iniciar Sesión</Nav.Link>
-            <Nav.Link href="#link">Cerrar Sesión</Nav.Link>
+          <Nav className="ms-auto">
+            <NavLink end to={'/'} className={'nav-item nav-link'}>Inicio</NavLink>
+            <NavLink end to={"/registro"} className={'nav-item nav-link'}>Registro</NavLink>
+            <NavLink end to={"/login"} className={'nav-item nav-link'}>Iniciar Sesión</NavLink>
+            <NavLink end to={'/'} className={'nav-item nav-link'}>Cerrar Sesión</NavLink>
+            <NavLink end to={'/admin-pacientes'} className={'nav-item nav-link'}>Administrar pacientes</NavLink>
+            <NavLink end to={'/admin-turnos'} className={'nav-item nav-link'}>Administrar turnos</NavLink>
+            <NavLink end to={'/admin-servicios'} className={'nav-item nav-link'}>Administrar servicios</NavLink>
+            <NavLink end to={'/nosotros'} className={'nav-item nav-link'}>Sobre nosotros</NavLink>
+            <NavLink end to={'/contacto'} className={'nav-item nav-link'}>Contacto</NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
