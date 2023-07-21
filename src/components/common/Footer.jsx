b1daaf7880f57c63c32c818a12f9d84c4754c24f
RLauraS <R_LauraS@yahoo.com.ar>
2023-07-21 19:59:54 -0300

    Se agrega logo, teléfono, redes sociales

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,14 +1,25 @@
 import React from "react";
-import '../../App.css';
-
+import "../../App.css";
+import { Link } from "react-router-dom";
+import logo from "../../assets/Logo.png";
 const Footer = () => {
   return (
-    <footer className='fondoFooter fuente'>
+    <footer className="fondoFooter fuente">
       <div className="container">
         <nav className="row">
+          <Link
+            to="/"
+            className="clo-12 col-md-3 d-flex align-items-center justify-content-center"
+          >
+            <img src={logo} alt="Logo Rolling Vet" width='150' />
+          </Link>
+
+
           <ul className="col-12 col-md-3 list-unstyled">
-            <li className="mb-2"></li>
-            <li className="text-center"></li>
+            <li className="m-2 fs-2">¿Consultas?</li>
+            <li>
+            <i className="bi bi-telephone-fill fs-2"> 381-333-444</i>
+            </li>
           </ul>
           <ul className="col-12 col-md-3 list-unstyled">
             <li className="mb-2"></li>
@@ -24,9 +35,8 @@ const Footer = () => {
                 <i className="bi bi-instagram"></i>
               </a>
               <a href="https://twitter.com" target="_blank">
-              <i className="bi bi-twitter"></i>
+                <i className="bi bi-twitter"></i>
               </a>
-
             </li>
           </ul>
         </nav>
