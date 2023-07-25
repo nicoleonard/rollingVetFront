dd44b2d489d119a3db48123f601b2d25bc93a302
RLauraS <R_LauraS@yahoo.com.ar>
2023-07-24 23:32:42 -0300

    Se crea estructura básica de la pagin acerca de nosotros para completar

--- src/components/views/Nosotros.jsx
+++ src/components/views/Nosotros.jsx
@@ -1,7 +1,110 @@
+import React from "react";
+import '../../App.css';
+
 const Nosotros = () => {
   return (
     <section className="container mainSection">
-      <h1>Nosotros</h1>
+      <section class="text-center py-3">
+        <h1>Nosotros</h1>
+        <p class="ps-4 pe-4">
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam numquam facere ullam est magnam, soluta provident sapiente quaerat? Voluptate atque nobis pariatur ea nisi alias exercitationem cupiditate omnis veritatis eligendi?
+        </p>
+      </section>
+      <section class="container">
+        <article class="row">
+          <aside class="col-sm-6 col-md-6 col-lg-6">
+            <img
+              src=""
+              alt=""
+              width="100%"
+              class="my-4"
+            />
+          </aside>
+          <aside class="col-sm-6 col-md-6 col-lg-6 my-4">
+            <h5>Nombre:</h5>
+            <p></p>
+            <h5>Edad:</h5>
+            <p></p>
+            <h5>Puesto:</h5>
+            <p>Scrum Master / backend</p>
+            <h5>Roll</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minus consectetur repellat est eius nihil, ipsum, pariatur maiores quibusdam fuga omnis ut quidem illo similique veritatis itaque, blanditiis in labore?</p>
+          </aside>
+          <hr />
+          <aside class="col-sm-6 col-md-6 col-lg-6">
+            <img
+              src=""
+              alt=""
+              width="100%"
+              class="my-4"
+            />
+          </aside>
+          <aside class="col-sm-6 col-md-6 col-lg-6 my-4">
+            <h5>Nombre:</h5>
+            <p></p>
+            <h5>Edad:</h5>
+            <p></p>
+            <h5>Puesto:</h5>
+            <p>Scrum Master / backend</p>
+            <h5>Roll</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minus consectetur repellat est eius nihil, ipsum, pariatur maiores quibusdam fuga omnis ut quidem illo similique veritatis itaque, blanditiis in labore?</p>
+          </aside>
+          <hr />          <aside class="col-sm-6 col-md-6 col-lg-6">
+            <img
+              src=""
+              alt=""
+              width="100%"
+              class="my-4"
+            />
+          </aside>
+          <aside class="col-sm-6 col-md-6 col-lg-6 my-4">
+            <h5>Nombre:</h5>
+            <p></p>
+            <h5>Edad:</h5>
+            <p></p>
+            <h5>Puesto:</h5>
+            <p>Scrum Master / backend</p>
+            <h5>Roll</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minus consectetur repellat est eius nihil, ipsum, pariatur maiores quibusdam fuga omnis ut quidem illo similique veritatis itaque, blanditiis in labore?</p>
+          </aside>
+          <hr />          <aside class="col-sm-6 col-md-6 col-lg-6">
+            <img
+              src=""
+              alt=""
+              width="100%"
+              class="my-4"
+            />
+          </aside>
+          <aside class="col-sm-6 col-md-6 col-lg-6 my-4">
+            <h5>Nombre:</h5>
+            <p></p>
+            <h5>Edad:</h5>
+            <p></p>
+            <h5>Puesto:</h5>
+            <p>Scrum Master / backend</p>
+            <h5>Roll</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minus consectetur repellat est eius nihil, ipsum, pariatur maiores quibusdam fuga omnis ut quidem illo similique veritatis itaque, blanditiis in labore?</p>
+          </aside>
+          <hr />          <aside class="col-sm-6 col-md-6 col-lg-6">
+            <img
+              src=""
+              alt=""
+              width="100%"
+              class="my-4"
+            />
+          </aside>
+          <aside class="col-sm-6 col-md-6 col-lg-6 my-4">
+            <h5>Nombre:</h5>
+            <p></p>
+            <h5>Edad:</h5>
+            <p></p>
+            <h5>Puesto:</h5>
+            <p>Scrum Master / backend</p>
+            <h5>Roll</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minus consectetur repellat est eius nihil, ipsum, pariatur maiores quibusdam fuga omnis ut quidem illo similique veritatis itaque, blanditiis in labore?</p>
+          </aside>
+        </article>
+      </section>
     </section>
   );
 };
