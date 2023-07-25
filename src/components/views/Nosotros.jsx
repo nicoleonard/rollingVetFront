97fd3eb3ea4bfde970dc66fa3be63cb72fc2ed11
RLauraS <R_LauraS@yahoo.com.ar>
2023-07-25 00:27:08 -0300

    Se crea estructura básica de la pagin acerca de nosotros para completar con color de fondo plano

--- src/components/views/Nosotros.jsx
+++ src/components/views/Nosotros.jsx
@@ -1,24 +1,22 @@
 import React from "react";
-import '../../App.css';
+import "../../App.css";
 
 const Nosotros = () => {
   return (
-    <section className="container mainSection">
+    <section className="container mainSection fondo fondoSinImagen">
       <section class="text-center py-3">
         <h1>Nosotros</h1>
         <p class="ps-4 pe-4">
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam numquam facere ullam est magnam, soluta provident sapiente quaerat? Voluptate atque nobis pariatur ea nisi alias exercitationem cupiditate omnis veritatis eligendi?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam numquam
+          facere ullam est magnam, soluta provident sapiente quaerat? Voluptate
+          atque nobis pariatur ea nisi alias exercitationem cupiditate omnis
+          veritatis eligendi?
         </p>
       </section>
       <section class="container">
         <article class="row">
           <aside class="col-sm-6 col-md-6 col-lg-6">
-            <img
-              src=""
-              alt=""
-              width="100%"
-              class="my-4"
-            />
+            <img src="" alt="" width="100%" class="my-4" />
           </aside>
           <aside class="col-sm-6 col-md-6 col-lg-6 my-4">
             <h5>Nombre:</h5>
@@ -28,16 +26,16 @@ Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam numquam facere ul
             <h5>Puesto:</h5>
             <p>Scrum Master / backend</p>
             <h5>Roll</h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minus consectetur repellat est eius nihil, ipsum, pariatur maiores quibusdam fuga omnis ut quidem illo similique veritatis itaque, blanditiis in labore?</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Aspernatur minus consectetur repellat est eius nihil, ipsum,
+              pariatur maiores quibusdam fuga omnis ut quidem illo similique
+              veritatis itaque, blanditiis in labore?
+            </p>
           </aside>
           <hr />
           <aside class="col-sm-6 col-md-6 col-lg-6">
-            <img
-              src=""
-              alt=""
-              width="100%"
-              class="my-4"
-            />
+            <img src="" alt="" width="100%" class="my-4" />
           </aside>
           <aside class="col-sm-6 col-md-6 col-lg-6 my-4">
             <h5>Nombre:</h5>
@@ -47,33 +45,16 @@ Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam numquam facere ul
             <h5>Puesto:</h5>
             <p>Scrum Master / backend</p>
             <h5>Roll</h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minus consectetur repellat est eius nihil, ipsum, pariatur maiores quibusdam fuga omnis ut quidem illo similique veritatis itaque, blanditiis in labore?</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Aspernatur minus consectetur repellat est eius nihil, ipsum,
+              pariatur maiores quibusdam fuga omnis ut quidem illo similique
+              veritatis itaque, blanditiis in labore?
+            </p>
           </aside>
-          <hr />          <aside class="col-sm-6 col-md-6 col-lg-6">
-            <img
-              src=""
-              alt=""
-              width="100%"
-              class="my-4"
-            />
-          </aside>
-          <aside class="col-sm-6 col-md-6 col-lg-6 my-4">
-            <h5>Nombre:</h5>
-            <p></p>
-            <h5>Edad:</h5>
-            <p></p>
-            <h5>Puesto:</h5>
-            <p>Scrum Master / backend</p>
-            <h5>Roll</h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minus consectetur repellat est eius nihil, ipsum, pariatur maiores quibusdam fuga omnis ut quidem illo similique veritatis itaque, blanditiis in labore?</p>
-          </aside>
-          <hr />          <aside class="col-sm-6 col-md-6 col-lg-6">
-            <img
-              src=""
-              alt=""
-              width="100%"
-              class="my-4"
-            />
+          <hr />{" "}
+          <aside class="col-sm-6 col-md-6 col-lg-6">
+            <img src="" alt="" width="100%" class="my-4" />
           </aside>
           <aside class="col-sm-6 col-md-6 col-lg-6 my-4">
             <h5>Nombre:</h5>
@@ -83,15 +64,16 @@ Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam numquam facere ul
             <h5>Puesto:</h5>
             <p>Scrum Master / backend</p>
             <h5>Roll</h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minus consectetur repellat est eius nihil, ipsum, pariatur maiores quibusdam fuga omnis ut quidem illo similique veritatis itaque, blanditiis in labore?</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Aspernatur minus consectetur repellat est eius nihil, ipsum,
+              pariatur maiores quibusdam fuga omnis ut quidem illo similique
+              veritatis itaque, blanditiis in labore?
+            </p>
           </aside>
-          <hr />          <aside class="col-sm-6 col-md-6 col-lg-6">
-            <img
-              src=""
-              alt=""
-              width="100%"
-              class="my-4"
-            />
+          <hr />{" "}
+          <aside class="col-sm-6 col-md-6 col-lg-6">
+            <img src="" alt="" width="100%" class="my-4" />
           </aside>
           <aside class="col-sm-6 col-md-6 col-lg-6 my-4">
             <h5>Nombre:</h5>
@@ -101,7 +83,31 @@ Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam numquam facere ul
             <h5>Puesto:</h5>
             <p>Scrum Master / backend</p>
             <h5>Roll</h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur minus consectetur repellat est eius nihil, ipsum, pariatur maiores quibusdam fuga omnis ut quidem illo similique veritatis itaque, blanditiis in labore?</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Aspernatur minus consectetur repellat est eius nihil, ipsum,
+              pariatur maiores quibusdam fuga omnis ut quidem illo similique
+              veritatis itaque, blanditiis in labore?
+            </p>
+          </aside>
+          <hr />{" "}
+          <aside class="col-sm-6 col-md-6 col-lg-6">
+            <img src="" alt="" width="100%" class="my-4" />
+          </aside>
+          <aside class="col-sm-6 col-md-6 col-lg-6 my-4">
+            <h5>Nombre:</h5>
+            <p></p>
+            <h5>Edad:</h5>
+            <p></p>
+            <h5>Puesto:</h5>
+            <p>Scrum Master / backend</p>
+            <h5>Roll</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Aspernatur minus consectetur repellat est eius nihil, ipsum,
+              pariatur maiores quibusdam fuga omnis ut quidem illo similique
+              veritatis itaque, blanditiis in labore?
+            </p>
           </aside>
         </article>
       </section>
