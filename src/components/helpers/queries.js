c7d8b690fb768813a20365fcf10f69fc8bb4c979
Santiago Antich <santiantich98@gmail.com>
2023-07-25 15:21:19 -0300

    Terminé la lógica de Login.jsx y las queries de login en queries.js

--- src/components/helpers/queries.js
+++ src/components/helpers/queries.js
@@ -1,6 +1,20 @@
 // llamar la variable de entorno
 const URL_usuario = import.meta.env.VITE_API_USUARIO;
 
-export const login = ()=>{
-    console.log(URL_usuario);
+export const login = async (usuario)=>{
+    try{
+        //pedir a la api la lista de usuarios
+        const respuesta = await fetch(URL_usuario,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-token': JSON.parse(sessionStorage.getItem('usuario')).token
+            },
+            body: JSON.stringify(usuario)
+        });
+        const usuarioLogueado = await respuesta.json();
+        return usuarioLogueado;
+    }catch(error){
+        console.log(error)
+    }
 }
--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -1,33 +1,84 @@
-import { Container } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+import { Form, Button, Container, Card } from "react-bootstrap";
 import { login } from "../helpers/queries";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
-function Login() {
+const Login = ({setUsuarioLogueado}) => {
+  const { register, handleSubmit, formState: { errors}, reset } = useForm();
+  const navegacion = useNavigate();
 
-  login();
-  
+
+const onSubmit = (usuario)=>{
+  login(usuario).then((respuesta)=>{
+    console.log(respuesta)
+    if(respuesta && respuesta.nombreUsuario){
+      //guardar mi usuario en session o localstorage.
+      sessionStorage.setItem('usuario', JSON.stringify(respuesta));
+      setUsuarioLogueado(respuesta);
+      navegacion('/administrador');
+    }else{
+      //mostrar un mensaje  de error
+      Swal.fire(
+        'Ocurrio un error',
+        'El email o password son erroneos',
+        'error'
+      )
+    }
+  })
+}
+
   return (
     <Container className="mainSection">
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+      <Card className="my-5">
+        <Card.Header as="h5">Login</Card.Header>
+        <Card.Body>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Ingrese un email"
+               { ...register('email', {
+                required:'El email es un dato obligatorio',
+                pattern: {
+                  value: /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+                  message:'El email debe tener un formato valido (mail@dominio.com)'
+                }
+               })}
+              />
+              <Form.Text className="text-danger">
+               { errors.email?.message}
+              </Form.Text>
+            </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+               {
+                ...register('password',{
+                  required: 'El password es un dato obligatorio',
+                  pattern:{
+                    value: /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/,
+                    message: 'El password debe contener entre 8 y 16 caracteres y debe incluir numeros, caracteres en mayuscula, miniscula y almenos un caracter especial'
+                  }
+                })
+               }
+              />
+             <Form.Text className="text-danger">
+               { errors.password?.message}
+              </Form.Text>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Ingresar
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
     </Container>
   );
-}
+};
 
 export default Login;
