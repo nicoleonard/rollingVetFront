5121e5ae64ec499d5d7edf63376f7fa6a153e5ea
Facund05 <118226317+FacundoCajal199@users.noreply.github.com>
2023-07-26 16:57:37 -0300

    agregamos la funcion que va obtener y mostrar los pacientes

--- src/components/views/AdminPacientes.jsx
+++ src/components/views/AdminPacientes.jsx
@@ -1,9 +1,22 @@
+
+
 const AdminPacientes = () => {
-  return (
-    <section className="container mainSection">
-      <h1>Administrador de Pacientes</h1>
-    </section>
-  );
-};
+  const [productos, setProductos] = useState([]);
+
+  useEffect(() => {
+    obtenerProductos().then((respuesta) => {
+      if (respuesta) {
+        setProductos(respuesta);
+      } else {
+        Swal.fire(
+          "Ocurrio un error",
+          "Intente realizar esta operacion en unos minutos",
+          "error"
+        );
+      }
+    });
+  }, []);
+
+  
 
 export default AdminPacientes;
