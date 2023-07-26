a9a6c08ef466a351014d6e8b194e6f58ef21fd9b
Matias G <dikimatig@gmail.com>
2023-07-26 07:46:02 -0300

    crud usuarios terminado faltan establecer los nombre correctamente

---
+++ src/components/views/AdminUsuarios.jsx
@@ -0,0 +1,44 @@
+import { Container, Table } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { leerUsuarios } from "../helpers/queries";
+import AdminUsuario from "./usuarios/AdminUsuario";
+
+const AdminUsuarios = () => {
+  const [usuarios, setUsuarios] = useState([]);
+
+  useEffect(() => {
+    leerUsuarios().then((respuesta) => {
+      if (respuesta) {
+        setUsuarios(respuesta);
+      } else {
+        Swal.fire("Oops...", "Intente esta operacion luego", "error");
+      }
+    });
+  }, []);
+
+  return (
+    <Container fluid="true" className="px-5">
+      <div className="d-flex justify-content-between align-items-center mt-5">
+        <h1 className="display-4 ">Lista de usuarios</h1>
+      </div>
+      <hr />
+      <Table responsive striped bordered hover>
+        <thead>
+          <tr>
+            <th>Codigo</th>
+            <th>Usuario</th>
+            <th>Email</th>
+            <th>Tipo</th>
+          </tr>
+        </thead>
+        <tbody>
+          {usuarios.map((usuario) => (<AdminUsuario usuario={usuario} key={usuario._id} setUsuarios={setUsuarios}
+            ></AdminUsuario>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+  );
+};
+
+export default AdminUsuarios;
---
+++ src/components/views/usuarios/AdminUsuario.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+//import { Button } from "react-bootstrap";
+//import { borrarUsuario, leerUsuarios } from "../../helpers/queries";
+const AdminUsuario = ({ usuario, setUsuarios }) => {
+  const eliminarUsuario = () => {
+    Swal.fire({
+      title: "Esta a punto de borrar un usuario, esta seguro?",
+      text: "No hay vuelta atras!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#117700",
+      cancelButtonColor: "#ff2255",
+      confirmButtonText: "Si, borrar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        borrarUsuario(usuario._id).then((respuesta) => {
+          if (respuesta && respuesta.status === 200) {
+            Swal.fire(
+              "Se ha ido!",
+              `El Usuario ${usuario.nombre} se ha borrado`,
+              "success"
+            );
+            leerUsuarios().then((respuesta) => {
+              if (respuesta) {
+                setUsuarios(respuesta);
+              }
+            });
+          } else {
+            Swal.fire(
+              "Oh no! Algo salio mal...",
+              `El usuario ${usuario.nombre} no se ha borrado`,
+              "error"
+            );
+          }
+        });
+      }
+    });
+  };
+  return (
+    <tr>
+      <td>{usuario._id}</td>
+      <td>{usuario.usuario}</td>
+      <td>{usuario.email}</td>
+      <td>{usuario.tipo}</td>
+      <td className="d-flex flex-columns flex-wrap justify-content-center">
+        <Button className="w-75" variant="danger" onClick={eliminarUsuario}>
+          Borrar
+        </Button>
+      </td>
+    </tr>
+  );
+};
+
+export default AdminUsuario;
