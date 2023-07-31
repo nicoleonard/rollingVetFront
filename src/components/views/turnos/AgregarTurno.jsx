82e7de2962572c485ff53e456b6e4609607ce7ac
nicoleonard <nikolasleonard@gmail.com>
2023-07-31 01:37:15 -0300

    modificado componente AgregarTurno parar permitir que use parametros de su ruta y los cargue en el formulario

--- src/components/views/turnos/AgregarTurno.jsx
+++ src/components/views/turnos/AgregarTurno.jsx
@@ -1,25 +1,26 @@
 import { Form, Button, Container } from "react-bootstrap";
 import { useForm } from "react-hook-form";
-/* import { agregarTurno } from "../../helpers/queriesTurnos";*/
+import { agregarTurno } from "../../helpers/queriesTurnos";
+
 const AgregarTurno = () => {
+  
   const {
     register,
     handleSubmit,
     formState: { errors },
+    setValue,
     reset,
   } = useForm();
+  const { hora, veterinario } = useParams();
+
+  useEffect(() => {
+        setValue('hora', hora)
+        setValue('veterinario',veterinario)
+  }, [])
+
+
 
   const onSubmit = (nuevoTurno) => {
-    checkTimeSlotAvailability(nuevoTurno);
-};
-const checkTimeSlotAvailability = (nuevoTurno) => {
-    // Make an API call to check if the time slot is available
-    // You can use a library like Axios or fetch to make the API call
-    // Assuming the API response returns an availability status
-    const isAvailable = true;
-
-    if (isAvailable) {
-      // Proceed with submitting the form
     agregarTurno(nuevoTurno).then((respuesta) => {
       if (respuesta && respuesta.status === 201) {
         Swal.fire(
@@ -36,14 +37,6 @@ const checkTimeSlotAvailability = (nuevoTurno) => {
         );
       }
     });
-  } else {
-    // Display error message to the user that the time slot is already booked
-    Swal.fire(
-      "Oops... algo salio mal",
-      "El horario seleccionado ya ha sido reservado, por favor elija otro horario",
-      "error"
-    );
-  }
 };
 
   return (
@@ -53,42 +46,24 @@ const checkTimeSlotAvailability = (nuevoTurno) => {
       <Form onSubmit={handleSubmit(onSubmit)}>
         <Form.Group className="mb-3" controlId="formHorario">
             <Form.Label>Horario</Form.Label>
-            <Form.Select 
-            {...register("horario", {
-                required: "El horario del turno no puede estar vacio",
-                validate: {
-                    isAvailable: (value) => checkTimeSlotAvailability(value),
-                }
+            <Form.Control type="time"
+                          readOnly
+            {...register("hora", {
+                required: "La hora del turno no puede estar vacia",
             })}>
-        <option>Seleccione su horario</option>
-        <option value="1">9:00</option>
-        <option value="2">9:30</option>
-        <option value="3">10:00</option>
-        <option value="4">10:30</option>
-        <option value="5">11:00</option>
-        <option value="6">11:30</option>
-        <option value="7">12:00</option>
-        <option value="8">12:30</option>
-        <option value="9">13:00</option>
-        <option value="10">13:30</option>
-        <option value="11">14:00</option>
-        <option value="12">14:30</option>
-        <option value="13">15:00</option>
-        <option value="14">15:30</option>
-        <option value="15">16:00</option>
-        <option value="16">16:30</option>
-        </Form.Select>
-        <Form.Text className="text-danger">
-            {errors.horario?.message}
-          </Form.Text>
+            </Form.Control>
+            <Form.Text className="text-danger">
+              {errors.hora?.message}
+            </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formDoctor">
-        <Form.Label>Doctor</Form.Label>
-            <Form.Select>
-        <option>Seleccione su doctor</option>
-        <option value="1">Dr Pablo</option>
-        <option value="2">Dr Mariano</option>
-        </Form.Select>
+        <Form.Label>Veterinario</Form.Label>
+            <Form.Control type="time"
+                          readOnly
+            {...register("veterinario", {
+                required: "El campo del veterinario no puede estar vacio",
+            })}>
+            </Form.Control>
         <Form.Text className="text-danger">
             {errors.doctor?.message}
           </Form.Text>
