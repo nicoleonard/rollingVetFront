50c6cad48a8c026979514f978c4e3d118fc6d619
RLauraS <R_LauraS@yahoo.com.ar>
2023-08-01 17:51:41 -0300

    Se crean las cards de manera de se completen por porps

--- src/components/views/inicio/CardInicio.jsx
+++ src/components/views/inicio/CardInicio.jsx
@@ -1,17 +1,17 @@
+import React from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function CardInicio() {
+function CardInicio({ titulo, contenido, imagen }) {
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+      <Card.Img variant="top" src={imagen} style={{ height: '250px', objectFit: 'cover' }} />
+      <Card.Body className="contenedor-recuadro ">
+        <Card.Title>{titulo}</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          {contenido}
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="success">Precio y variedades</Button>
       </Card.Body>
     </Card>
   );
