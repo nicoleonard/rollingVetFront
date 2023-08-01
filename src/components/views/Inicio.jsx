5a111d3812530f6087220b89d3a50ebf07dee5fb
Santiago Antich <santiantich98@gmail.com>
2023-08-01 00:02:18 -0300

    comenzamos a trabajar en la página de inicio

--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -1,8 +1,32 @@
+import { Container, Row, Col } from "react-bootstrap";
+import CardInicio from "./inicio/CardInicio";
+import CarrouseInicio from "./inicio/CarrouseInicio";
+
+
 const Inicio = () => {
+  /* const [servicios,setServicios] = useState([])
+  useEffect(()=>{
+    obtenerProductos().then(res=>{
+      setProductos(res)
+    })
+  },[]) */
   return (
-    <section className="container mainSection">
-      <h1>Inicio</h1>
-    </section>
+    <>
+    <CarrouseInicio></CarrouseInicio>
+    <h2 className="text-center display-1">Productos a la venta</h2>
+    <Container className="text-center my-5">
+    <Row className="mb-5">
+      <Col><CardInicio></CardInicio></Col>
+      <Col><CardInicio></CardInicio></Col>
+      <Col><CardInicio></CardInicio></Col>
+    </Row>
+    <Row>
+    <Col><CardInicio></CardInicio></Col>
+    <Col><CardInicio></CardInicio></Col>
+    <Col><CardInicio></CardInicio></Col>
+    </Row>
+    </Container>
+    </>
   );
 };
 
