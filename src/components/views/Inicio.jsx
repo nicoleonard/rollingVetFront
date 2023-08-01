38fcac2062dc1733efad5c50a6dfe6b2eea0e819
Santiago Antich <santiantich98@gmail.com>
2023-08-01 00:50:27 -0300

    Hicimos el read de servicios en el componente inicio

--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -1,19 +1,22 @@
 import { Container, Row, Col } from "react-bootstrap";
 import CardInicio from "./inicio/CardInicio";
 import CarrouseInicio from "./inicio/CarrouseInicio";
+import ListaServicio from "./servicios/ListaServicio";
+import { useEffect, useState } from "react";
+import { leerServicios } from "../helpers/queriesServicios";
 
 
 const Inicio = () => {
-  /* const [servicios,setServicios] = useState([])
+  const [servicios,setServicios] = useState([])
   useEffect(()=>{
-    obtenerProductos().then(res=>{
-      setProductos(res)
+    leerServicios().then(res=>{
+      setServicios(res)
     })
-  },[]) */
+  },[])
   return (
     <>
     <CarrouseInicio></CarrouseInicio>
-    <h2 className="text-center display-1">Productos a la venta</h2>
+    <h2 className="text-center display-1">Productos</h2>
     <Container className="text-center my-5">
     <Row className="mb-5">
       <Col><CardInicio></CardInicio></Col>
@@ -26,6 +29,20 @@ const Inicio = () => {
     <Col><CardInicio></CardInicio></Col>
     </Row>
     </Container>
+    <Container>
+  <h1 className="display-4">Nuestros Servicios</h1>
+  <hr />
+  <Row>
+    {servicios && servicios.length > 0 ? (
+      servicios.map((servicio) => (
+        <ListaServicio key={servicio._id} servicio={servicio} />
+      ))
+    ) : (
+      <p>No hay servicios disponibles</p>
+    )}
+  </Row>
+</Container>
+
     </>
   );
 };
