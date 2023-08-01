40bbc765d217d2c77091911ab719dcd1b2ad0c84
Santiago Antich <santiantich98@gmail.com>
2023-08-01 18:05:24 -0300

    Le agregué clases a los nombres de los clientes en el componente reviewclientes

--- src/components/views/inicio/ReviewClientes.jsx
+++ src/components/views/inicio/ReviewClientes.jsx
@@ -5,16 +5,16 @@ const ReviewClientes = () => {
     <Container className="contenedor-recuadro mb-3">
       <h2>Reseñas de clientes</h2>
       <hr />
-      <h3>Alfonso: </h3>
+      <h3 className="reviewNombre">Alfonso: </h3>
       <p>
         Esta es mi veterinaria favorita. Nunca tuve problemas y siempre
         atendieron a mi caniche en tiempo y forma
       </p>
-      <h3>Hilda: </h3>
+      <h3 className="reviewNombre">Hilda: </h3>
       <p>
         El doctor Mariano es de lo mejor. El operó a mi perro y me dio mucha seguridad.
       </p>
-      <h3>Romina: </h3>
+      <h3 className="reviewNombre">Romina: </h3>
       <p>
         Precios muy baratos!!!
       </p>
