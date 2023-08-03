097058240c713e4aebea4ad1c2927d5a44846eae
nicoleonard <nikolasleonard@gmail.com>
2023-08-03 02:15:54 -0300

    Cambios en la logica para generar la lista de turnos reservados

--- src/components/views/AdminTurnos.jsx
+++ src/components/views/AdminTurnos.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
-import { Container, Col, Button } from "react-bootstrap";
+import { Container, Col, Row, Button } from "react-bootstrap";
 import ReservaTurno from "./turnos/ReservaTurno"
 import { leerTurnos, borrarTurnos, inicializarTurnos } from "../helpers/queriesTurnos";
 import Swal from "sweetalert2";
-import { Link } from "react-router-dom";
+
 
 
 const AdminTurnos = () => {
@@ -26,7 +26,7 @@ const AdminTurnos = () => {
     if (isLoading) {
       borrarTurnos().then((respuesta) => {
         if (respuesta && respuesta.status === 200) {
-          inicializarTurnos(hora.length,veterinarios).then((respuesta) => {
+          inicializarTurnos(hora.length, veterinarios).then((respuesta) => {
             if (respuesta && respuesta.status === 201) {
               Swal.fire(
                 "Operacion exitosa!",
@@ -72,53 +72,42 @@ const AdminTurnos = () => {
   };
 
   let hora = ["8:00", "9:00", "10:00", "11:00", "12:00", "17:00", "18:00", "19:00"]
-  const veterinarios = ["veterinario1","veterinario2"]
+  const veterinarios = ["veterinario1", "veterinario2"]
 
-
-  const generarLista = (horario, veterinario) => {
-    for (let i = 0; i < hora.length; i++) {
-      turnos.map((turno) => {
-        //funcion que se fije si esta libre el turno
-        if (turno.turnoLibre) {
-          return (
-            <Link className="btn btn-primary" to={'/admin-turnos/agregar-turno/' + i + veterinarios[1]}>
-              <p>${horario}</p>
-            </Link>
-          )
-        }
-
-        if (turno.veterinario.includes('veterinario1')) {
-          if (turno.hora === horario[i]) {
-            return <ReservaTurno turno={turno} setTurnos={setTurnos} key={turno._id} hora={horario[i]}></ReservaTurno>
-          }
-        }
-      })
-
-
-
-    }
-  }
   return (
     <>
       <h1 className='text-center'>Lista de reservas de turnos</h1>
       <hr></hr>
-      <Container className='d-flex'>
-        <Button variant="danger" disabled={isLoading} onClick={!isLoading ? handleClick : null}>
-          {isLoading ? 'Procesando…' : 'Inicializar Turnos'}
-        </Button>
-        <Col className='w-50'>
-          <h2>Veterinario 1</h2>
+      <Container>
+        <Row>
+          <Button variant="danger" disabled={isLoading} onClick={!isLoading ? handleClick : null}>
+            {isLoading ? 'Procesando…' : 'Inicializar Turnos'}
+          </Button>
+        </Row>
+        <Row>
+          <Col className='w-50'>
+            <h2>Veterinario 1</h2>
+            {
+              turnos.map((turno) => {
+                if (turno.veterinario === veterinarios[0]) {
+                  return <ReservaTurno turno={turno} setTurnos={setTurnos} key={turno._id} hora={turno.hora}></ReservaTurno>
+                }
+              })
 
-          {
-            generarLista(hora, veterinarios)
-          }
-        </Col>
-        <Col className='w-50'>
-          <h2>Veterinario 2</h2>
-          {
-            generarLista(hora, veterinarios)
-          }
-        </Col>
+            }
+          </Col>
+          <Col className='w-50'>
+            <h2>Veterinario 2</h2>
+            {
+              turnos.map((turno) => {
+                if (turno.veterinario === veterinarios[1]) {
+                  return <ReservaTurno turno={turno} setTurnos={setTurnos} key={turno._id} hora={turno.hora}></ReservaTurno>
+                }
+              })
+            }
+          </Col>
+        </Row>
+
       </Container>
 
 
