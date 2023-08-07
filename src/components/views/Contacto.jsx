97de57cbbfa38d8b4261dd6bb2993f04110a8acf
Facund05 <118226317+FacundoCajal199@users.noreply.github.com>
2023-08-07 12:36:02 -0300

    contacto

--- src/components/views/Contacto.jsx
+++ src/components/views/Contacto.jsx
@@ -1,9 +1,28 @@
+import Form from 'react-bootstrap/Form';
+import { Button } from 'react-bootstrap';
 const Contacto = () => {
   return (
-    <section className="container mainSection">
-      <h1>Contacto</h1>
-    </section>
+
+    <Form className='container mainSection my-5'>
+      <h1>
+        Dejanos tus datos con tu consulta y nosotros te contactaremos
+      </h1>
+      <Form.Group className="mb-3">
+        <Form.Label>Nombre</Form.Label>
+        <Form.Control type="text"/>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="name@example.com" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Escribe tu consulta aqui</Form.Label>
+        <Form.Control as="textarea" rows={3} />
+      </Form.Group>
+      <Button variant="dark">enviar</Button>
+    </Form>
   );
 };
 
 export default Contacto;
+
