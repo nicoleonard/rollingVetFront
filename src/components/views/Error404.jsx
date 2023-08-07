e0651b7d00a3633a785d9aee2a98a5b04cf95f83
Santiago Antich <santiantich98@gmail.com>
2023-08-07 18:37:48 -0300

    cambios en error404

--- src/components/views/Error404.jsx
+++ src/components/views/Error404.jsx
@@ -1,16 +1,16 @@
-import { Button } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
 import React from "react";
 import "../../App.css";
 
 const Error404 = () => {
   return (
-      <section className="container-fluid mainSection fondo fondoSinImagen" >
+      <Container className="mainSection min-vh-100 fondo" >
       <div className="mb-2 btnCentrado pt-2">
         <Button href="/" variant='primary' size="lg">
           Volver
         </Button>
       </div>
-      </section>
+      </Container>
   );
 };
 
