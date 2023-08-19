b1aff81ddf6bbf6161b58500fd1d09553c5ea3e2
RLauraS <R_LauraS@yahoo.com.ar>
2023-08-18 21:12:40 -0300

    Se ajusta la posición de los planes para que se vean centrados en todos los dispositivos y se les da margen entre ellos

--- src/components/views/inicio/PlanesInicio.jsx
+++ src/components/views/inicio/PlanesInicio.jsx
@@ -8,47 +8,55 @@ const PlanesInicio = () => {
       <h3 className="textoAntiqueWhite">La mejor atención para tu mascota</h3>
       <hr />
       <Container className="my-5">
-        <Row>
+        <Row className="justify-content-center">
           <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Body className="fondoVerdeOscuro">
+            <Card style={{ width: "18rem" }} className="mx-auto mb-3 d-flex">
+              <Card.Body className="fondoVerdeOscuro text-center">
                 <Card.Title>
                   <div className="textoAntiqueWhite text-decoration-underline">
                     Primeros pasos
                   </div>
                 </Card.Title>
                 <Card.Text className="textoAntiqueWhite">
-                    Servicios para mascotas de 0 a 5 años.
-                    </Card.Text>
-                <Button as={Link} to="/contacto" variant="success">Adquirir</Button>
+                  Servicios para mascotas de 0 a 5 años.
+                </Card.Text>
+                <Button as={Link} to="/contacto" variant="success">
+                  Adquirir
+                </Button>
               </Card.Body>
             </Card>
           </Col>
           <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Body className="fondoVerdeOscuro">
+            <Card style={{ width: "18rem" }} className="mx-auto mb-3 d-flex">
+              <Card.Body className="fondoVerdeOscuro text-center">
                 <Card.Title>
                   <div className="textoAntiqueWhite text-decoration-underline">
                     Madurando
                   </div>
                 </Card.Title>
-                <Card.Text className="textoAntiqueWhite">Servicios para mascotas de 5 a 10 años.</Card.Text>
-                <Button as={Link} to="/contacto" variant="success">Adquirir</Button>
+                <Card.Text className="textoAntiqueWhite">
+                  Servicios para mascotas de 5 a 10 años.
+                </Card.Text>
+                <Button as={Link} to="/contacto" variant="success">
+                  Adquirir
+                </Button>
               </Card.Body>
             </Card>
           </Col>
           <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Body className="fondoVerdeOscuro">
+            <Card style={{ width: "18rem" }} className="mx-auto d-flex">
+              <Card.Body className="fondoVerdeOscuro text-center">
                 <Card.Title>
-                <div className="textoAntiqueWhite text-decoration-underline">
+                  <div className="textoAntiqueWhite text-decoration-underline">
                     Adultos
                   </div>
-                  </Card.Title>
+                </Card.Title>
                 <Card.Text className="textoAntiqueWhite">
                   Servicios para mascotas de más de 10 años.
                 </Card.Text>
-                <Button as={Link} to="/contacto" variant="success">Adquirir</Button>
+                <Button as={Link} to="/contacto" variant="success">
+                  Adquirir
+                </Button>
               </Card.Body>
             </Card>
           </Col>
